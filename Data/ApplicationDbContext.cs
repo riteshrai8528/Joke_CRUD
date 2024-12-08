81923b7507547d2bcb2e0f82e1cbd2067bd44191
riteshrai8528 <ritesh.20scse1010061@galgotiasuniversity.edu.in>
2024-12-08 19:52:16 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Joke_CRUD.Models;
+
+namespace Joke_CRUD.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Joke_CRUD.Models.Joke> Joke { get; set; } = default!;
+    }
+}
